302cf5e1cc3796bce29f63c09521e8c7518de096
Will Oke <Woke@lcs.on.ca>
2023-11-06 14:52:40 -0500

    Initial Commit

---
+++ New reminders app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  New reminders app
+//
+//  Created by William Oke on 2023-11-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ New reminders app/New_reminders_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  New_reminders_appApp.swift
+//  New reminders app
+//
+//  Created by William Oke on 2023-11-06.
+//
+
+import SwiftUI
+
+@main
+struct New_reminders_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
